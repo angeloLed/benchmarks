af9c83f7b3a7edbf3e2f44832cfb47349f23e6f7
Angelo <angelolandino@hotmail.it>
2018-04-12 19:11:46 +0200

    refactory vanilla node

--- nodejs-vanilla/index.js
+++ nodejs-vanilla/index.js
@@ -5,7 +5,7 @@ const http = require('http');
 const url = require('url');
 
 // Controller
-const userController = new (require('./app/UserController'));
+const heatController = new (require('./app/HeatController'));
 
 const server = http.createServer().listen(80);
 server.on('request', function (req, res) {
@@ -33,13 +33,13 @@ server.on('request', function (req, res) {
 
         //sorting route ( if you want an router engine, maybe is not a vanilla test that you looking for )
         if(req.method == 'GET' && urlInfo.pathname == '/heats') {
-            userController.getAll(req, res);
+            heatController.getAll(req, res);
         }
         else if(req.method == 'POST' && urlInfo.pathname == '/heats') {
-            userController.store(req, res);
+            heatController.store(req, res);
         }
         else if(req.method == 'GET' && urlInfo.pathname == '/userHeats') {
-            userController.getAllUserHasHeatZone(req, res);
+            heatController.getAllUserHasHeatZone(req, res);
         }
         else {
             res.writeHead(404, {'Content-Type': 'application/json'});
