b5c5ee5a2dfb9205911cc582e5b7a5995bf45bd8
AngeloLed <angelo.landino@mosaicoon.com>
2018-04-23 17:47:00 +0200

    more rust

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,13 +1,21 @@
 extern crate iron;
+extern crate router;
 
 use iron::prelude::*;
 use iron::status;
+use router::Router;
+
 
 fn main() {
-    fn hello_world(_: &mut Request) -> IronResult<Response> {
-        Ok(Response::with((status::Ok, "Hello World!")))
+    let mut router = Router::new();           // Alternative syntax:
+    router.get("/", handler, "index");        // let router = router!(index: get "/" => handler,
+    router.get("/:query", handler, "query");  //                      query: get "/:query" => handler);
+
+    fn handler(req: &mut Request) -> IronResult<Response> {
+        let ref query = req.extensions.get::<Router>().unwrap().find("query").unwrap_or("/");
+        Ok(Response::with((status::Ok, *query)))
     }
 
-    let _server = Iron::new(hello_world).http("0.0.0.0:80").unwrap();
+    let _server = Iron::new(router).http("0.0.0.0:80").unwrap();
     println!("On 80");
 }
